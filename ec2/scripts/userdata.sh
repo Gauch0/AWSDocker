aebef82c609e4d7447b4bbf8dd84e112068f10cf
Gauch0 <matiaslorenzo466@gmail.com>
2022-03-13 17:10:23 -0300

    se agregaron la ui y al api

--- ec2/scripts/userdata.sh
+++ ec2/scripts/userdata.sh
@@ -1,6 +1,49 @@
 #!/bin/bash -v
-echo "userdata-start"
-sudo apt-get update -y
-sudo apt-get install -y nginx > /tmp/nginx.log
-sudo service nginx start
-echo "userdata-end"
+
+#UPDATE
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl tree
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+
+# INSTALL DOCKER
+curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -a -G docker ubuntu
+sudo systemctl enable docker
+cat <<EOF >/etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+
+#INSTALL K8S
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF >/tmp/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo mv /tmp/kubernetes.list /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
+sudo systemctl enable kubelet
+sudo kubeadm config images pull
+
+#GIT CLONE
+
+mkdir /home/RampUP
+cd /home/RampUP
+sudo git clone 
