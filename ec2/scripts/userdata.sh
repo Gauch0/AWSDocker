c2797f5666051af92d6fd710e24ecf97ed5c5764
Gauch0 <matiaslorenzo466@gmail.com>
2022-03-18 06:13:03 -0700

    test

--- ec2/scripts/userdata.sh
+++ ec2/scripts/userdata.sh
@@ -2,72 +2,72 @@
 
 #UPDATE
 
-sudo apt-get upgrade
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl tree
+# sudo apt-get upgrade
+# sudo apt-get update
+# sudo apt-get install -y apt-transport-https ca-certificates curl tree
 
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl
+# sudo apt-get update
+# sudo apt-get install -y apt-transport-https ca-certificates curl
 
-# INSTALL DOCKER
-curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-sudo usermod -a -G docker ubuntu
-sudo systemctl enable docker
-cat <<EOF >/etc/docker/daemon.json
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2"
-}
-EOF
-sudo systemctl daemon-reload
-sudo systemctl restart docker
+# # INSTALL DOCKER
+# curl gnupg-agent software-properties-common
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+# sudo apt-get update
+# sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+# sudo usermod -a -G docker ubuntu
+# sudo systemctl enable docker
+# cat <<EOF >/etc/docker/daemon.json
+# {
+#   "exec-opts": ["native.cgroupdriver=systemd"],
+#   "log-driver": "json-file",
+#   "log-opts": {
+#     "max-size": "100m"
+#   },
+#   "storage-driver": "overlay2"
+# }
+# EOF
+# sudo systemctl daemon-reload
+# sudo systemctl restart docker
 
-#INSTALL K8S
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-cat <<EOF >/tmp/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-sudo mv /tmp/kubernetes.list /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
-echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
-sudo sysctl -p
-sudo systemctl enable kubelet
-sudo kubeadm config images pull
+# #INSTALL K8S
+# curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+# cat <<EOF >/tmp/kubernetes.list
+# deb https://apt.kubernetes.io/ kubernetes-xenial main
+# EOF
+# sudo mv /tmp/kubernetes.list /etc/apt/sources.list.d/kubernetes.list
+# sudo apt-get update
+# sudo apt-get install -y kubelet kubeadm kubectl
+# echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
+# sudo sysctl -p
+# sudo systemctl enable kubelet
+# sudo kubeadm config images pull
 
-#INSTALL MYSQL 
+# #INSTALL MYSQL 
 
-sudo apt-get update
-mkdir /home/mysql
-cd /home/mysql
-sudo apt install mysql-server
-sudo mysql_secure_installation
+# sudo apt-get update
+# mkdir /home/mysql
+# cd /home/mysql
+# sudo apt install mysql-server
+# sudo mysql_secure_installation
 
-#GIT CLONE
+# #GIT CLONE
 
-mkdir /home/rampup
-cd /home/rampup
-sudo git clone https://github.com/Gauch0/AWSDocker.git k8s-docker-movieapi-movieui
-sudo chown -R rampup k8s-docker-movieapi-movieui
+# mkdir /home/rampup
+# cd /home/rampup
+# sudo git clone https://github.com/Gauch0/AWSDocker.git k8s-docker-movieapi-movieui
+# sudo chown -R rampup k8s-docker-movieapi-movieui
 
 
 sudo chmod 666 /var/run/docker.sock
 
 #DOCKER CONTAINER
 
-docker pull gauch0/mysql
+# docker pull gauch0/mysql
 
-docker pull gauch0/uifix
+# docker pull gauch0/uifix
 
-docker pull gauch0/apifix
+# docker pull gauch0/apifix
 
 . ./ip.sh
 
@@ -87,7 +87,7 @@ export MYSQL_DATABASE=movie_db
 
 #API
 
-export DB_HOST=${myip}
+export DB_HOST=$myip
 
 export DB_USER=admin
  
@@ -97,7 +97,7 @@ export DB_NAME=movie_db
 
 #UI
 
-export BACKEND_URL=${myip}:1000
+export BACKEND_URL=$myip:1000
 
 #RUN CONTAINERS
 
@@ -106,5 +106,3 @@ docker run -it --rm --name ${CONTAINER_NAME} -p ${HOST_PORT}:3306 -e MYSQL_USER=
 docker run -it --rm --name api -p 1000:3000 -e DB_HOST=$DB_HOST -e DB_USER=$DB_USER -e DB_PASS=$DB_PASS -e DB_NAME=$DB_NAME -d gauch0/apifix
 
 docker run -it --rm --name ui -p 80:8000 -e BACKEND_URL=$BACKEND_URL:1000 -d gauch0/uifix
-
-
