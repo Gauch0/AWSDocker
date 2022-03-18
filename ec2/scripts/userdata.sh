3112ef44133782ba57fc02b65ab94586c79517da
Gauch0 <matiaslorenzo466@gmail.com>
2022-03-18 06:14:55 -0700

    all

--- ec2/scripts/userdata.sh
+++ ec2/scripts/userdata.sh
@@ -1,34 +1,34 @@
 #!/bin/bash -v
 
-#UPDATE
+UPDATE
 
-# sudo apt-get upgrade
-# sudo apt-get update
-# sudo apt-get install -y apt-transport-https ca-certificates curl tree
+sudo apt-get upgrade
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl tree
 
-# sudo apt-get update
-# sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
 
-# # INSTALL DOCKER
-# curl gnupg-agent software-properties-common
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-# sudo apt-get update
-# sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-# sudo usermod -a -G docker ubuntu
-# sudo systemctl enable docker
-# cat <<EOF >/etc/docker/daemon.json
-# {
-#   "exec-opts": ["native.cgroupdriver=systemd"],
-#   "log-driver": "json-file",
-#   "log-opts": {
-#     "max-size": "100m"
-#   },
-#   "storage-driver": "overlay2"
-# }
-# EOF
-# sudo systemctl daemon-reload
-# sudo systemctl restart docker
+# INSTALL DOCKER
+curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -a -G docker ubuntu
+sudo systemctl enable docker
+cat <<EOF >/etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+sudo systemctl daemon-reload
+sudo systemctl restart docker
 
 # #INSTALL K8S
 # curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
@@ -43,31 +43,31 @@
 # sudo systemctl enable kubelet
 # sudo kubeadm config images pull
 
-# #INSTALL MYSQL 
+#INSTALL MYSQL 
 
-# sudo apt-get update
-# mkdir /home/mysql
-# cd /home/mysql
-# sudo apt install mysql-server
-# sudo mysql_secure_installation
+sudo apt-get update
+mkdir /home/mysql
+cd /home/mysql
+sudo apt install mysql-server
+sudo mysql_secure_installation
 
-# #GIT CLONE
+#GIT CLONE
 
-# mkdir /home/rampup
-# cd /home/rampup
-# sudo git clone https://github.com/Gauch0/AWSDocker.git k8s-docker-movieapi-movieui
-# sudo chown -R rampup k8s-docker-movieapi-movieui
+mkdir /home/rampup
+cd /home/rampup
+sudo git clone https://github.com/Gauch0/AWSDocker.git k8s-docker-movieapi-movieui
+sudo chown -R rampup k8s-docker-movieapi-movieui
 
 
 sudo chmod 666 /var/run/docker.sock
 
 #DOCKER CONTAINER
 
-# docker pull gauch0/mysql
+docker pull gauch0/mysql
 
-# docker pull gauch0/uifix
+docker pull gauch0/uifix
 
-# docker pull gauch0/apifix
+docker pull gauch0/apifix
 
 . ./ip.sh
 
