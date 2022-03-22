d1eb1c6bcb06192c4a4ad45b7ace7e435ec1aaa0
Gauch0 <matiaslorenzo466@gmail.com>
2022-03-22 14:24:05 -0700

    DOCKER-COMPOSER FINISH

--- ec2/scripts/scheme.sql
+++ ec2/scripts/scheme.sql
@@ -1,7 +1,47 @@
+-- SET @user = "alex";
+
+-- CREATE SCHEMA `movie_db` ;
 use `movie_db`;
 
 
+CREATE TABLE `publications` (
+`name` VARCHAR(250) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
+`avatar` VARCHAR(250) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+PRIMARY KEY (`name`) USING BTREE
+)
+COLLATE='utf8mb4_0900_ai_ci'
+ENGINE=InnoDB
+;
 
+
+CREATE TABLE `reviewers` (
+`name` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
+`publication` VARCHAR(250) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+`avatar` VARCHAR(250) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+PRIMARY KEY (`name`) USING BTREE,
+INDEX `publication_fk` (`publication`) USING BTREE,
+CONSTRAINT `publication_fk` FOREIGN KEY (`publication`) REFERENCES `movie_db`.`publications` (`name`) ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+COLLATE='utf8mb4_0900_ai_ci'
+ENGINE=InnoDB
+;
+
+
+
+CREATE TABLE `movies` (
+`title` VARCHAR(250) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
+`release` VARCHAR(250) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+`score` INT NULL DEFAULT NULL,
+`reviewer` VARCHAR(250) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+PRIMARY KEY (`title`) USING BTREE,
+INDEX `reviewer_id` (`reviewer`) USING BTREE,
+CONSTRAINT `reviewer_id` FOREIGN KEY (`reviewer`) REFERENCES `movie_db`.`reviewers` (`name`) ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+COLLATE='utf8mb4_0900_ai_ci'
+ENGINE=InnoDB
+;
+
+use `movie_db`;
 
 -- INSERT into publications (name, avatar) VALUES('NombreRevista', 'imagenrevista');
 INSERT INTO publications (name, avatar) VALUES ('The Daily Reviewer', 'glyphicon-eye-open'),('International Movie Critic', 'glyphicon-fire'),('MoviesNow', 'glyphicon-time'),('MyNextReview', 'glyphicon-record'),('Movies Games', 'glyphicon-heart-empty'),('TheOne', 'glyphicon-globe'),('ComicBookHero.com', 'glyphicon-flash');
